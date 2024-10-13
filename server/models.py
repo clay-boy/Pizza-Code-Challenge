e14a3c7de8790dd6efc49bfe3cb241877179cbcc
Keith-M <keith.mwai@student.moringaschool.com>
2024-10-13 20:16:02 +0300

    add serialization to Pizza to avoid circular references

--- server/models.py
+++ server/models.py
@@ -37,8 +37,9 @@ class Pizza(db.Model, SerializerMixin):
 
     # add relationship
     restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy=True)
-    
+
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
